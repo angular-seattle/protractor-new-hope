8610be17aeb897be0e8f593b79d3a67d9f2a8dd0
Craig Nishina <craig.nishina@gmail.com>
2018-02-24 14:54:05 -0800

    Add assets and general tractor-beam control

--- src/app/tractor-control/tractor-control.component.ts
+++ src/app/tractor-control/tractor-control.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TractorControlComponent implements OnInit {
 
+  frameState = 1;
+  testState = 'pass'
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeTestState(newState: string) {
+    this.testState = newState;
+    this.resetFrameState();
+  }
+
+  incrementFrameState() {
+    if (this.frameState == 4)
+      return;
+    this.frameState++;
+  }
+
+  resetFrameState() {
+    this.frameState = 1;
+  }
+
+  getFrame(): string {
+    return `assets/${this.testState}/frame${this.frameState}.png`
+  }
 }
