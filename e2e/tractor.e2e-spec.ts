8200adf6a3e5eeb930effe70f5435e45f4ec1fb8
Craig Nishina <craig.nishina@gmail.com>
2018-03-31 13:35:21 -0700

    fix screenshot tests
    - update e2e golden screenshots with a 1280 x 900 png
    - set take_golden script to update the 'correct' screenshots and set
    back to a broken asset state.
    - include frame5.png to be the actual frame 4. set frame4.png to frame1
    for maximum diff.
    - update unit test to show view state vs component state changes for
    detect changes.

--- e2e/tractor.e2e-spec.ts
+++ e2e/tractor.e2e-spec.ts
@@ -4,22 +4,29 @@ import {browser, by, element, ExpectedConditions as EC} from 'protractor';
 import {compareScreenshot} from './screenshot_helper';
 
 const GOLDEN_IMAGES = [1,2,3,4].map((i) => {
-  return path.join(__dirname, `goldens/tractor_control${i}.png`)
+  return path.join(__dirname, `goldens/tractor_control${i}.png`);
 });
 
 describe('Tractor beam control page', () => {
 
   beforeEach(async() => {
     await browser.get('/tractor');
+    let width = 1280;
+    let height = 900;
+    await browser.driver.manage().window().setSize(width, height);
   });
 
   it('display the right images', async() => {
     console.log(GOLDEN_IMAGES)
     for(let golden of GOLDEN_IMAGES) {
-      console.log('Checking golden ', golden)
-      let data = await browser.takeScreenshot()
+      console.log('Checking golden ', golden);
+
+      // Get the screenshot and compare it to the golden.
+      let data = await browser.takeScreenshot();
       let result = await compareScreenshot(data, golden);
       expect(result).toBeTruthy();
+
+      // Increment the image.
       await element(by.css('img.lever')).click();
     }
   });
