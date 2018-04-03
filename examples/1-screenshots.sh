4f13a15678cb597de9fc946e9f3b17d048519882
Michael Giambalvo <heathkit@gmail.com>
2018-04-02 23:16:50 -0700

    Add some color to the debugger example script, add +x

--- examples/1-screenshots.sh
+++ examples/1-screenshots.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -u -e -o pipefail
+set +x -u -e -o pipefail
 
 # Only run the tractor beam specs
 export UPDATE_SCREENSHOTS='0'
--- examples/2-debugging.sh
+++ examples/2-debugging.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-set -u -e -o pipefail
+set +x -u -e -o pipefail
 
-$(npm bin)/protractor --specs='e2e/prisoners.e2e-spec.ts' --highlightDelay=1000
+BOLD=`tput bold`
+GREEN=`tput setaf 2`
+WHITE=`tput setaf 7`
+
+echo "${BOLD}Navigate to ${GREEN}chrome://inspect${WHITE} to open the inspector."
+tput sgr0
+
+# Set a ridiculously large timeout, so Jasmine doesn't throw an error while
+# we're paused in the debuggier
+JASMINE_TIMEOUT=999999 node --inspect-brk $(npm bin)/protractor --specs='e2e/prisoners.e2e-spec.ts'
