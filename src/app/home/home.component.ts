1fd92fc9304537f87872565ab8e26a996dd990a9
Michael Giambalvo <heathkit@gmail.com>
2018-04-05 00:35:10 -0700

    Styling improvements.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-  template: `
-  Welcome to the Death Star!
-  `
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
