8be7d7ad4b60e61a597965e1e0f3c2afb970a680
Michael Giambalvo <heathkit@gmail.com>
2018-04-10 10:25:15 -0700

    Get test placeholder ready for demo

--- e2e/firing-form.e2e-spec.ts
+++ e2e/firing-form.e2e-spec.ts
@@ -2,9 +2,7 @@ import {browser, by, element, ExpectedConditions as EC} from 'protractor';
 import {type, go, see, slow, click, under, leftOf, below, rightOf} from 'blue-harvest';
 
 async function waitForEnabled(label) {
-  browser.waitForAngularEnabled(false);
   await browser.wait(EC.elementToBeClickable(element(by.buttonText(label))));
-  browser.waitForAngularEnabled(true);
 }
 
 describe('Firing form', () => {
@@ -12,12 +10,8 @@ describe('Firing form', () => {
     await go('/firing');
   });
 
-  fit('should add a name with gold using action helpers', async() => {
+  it('should add a name with gold using action helpers', async() => {
     // Test goes here...
-
-    await waitForEnabled('Ready');
-    await click('Fire');
-    await slow.see('Destroyed');
   });
 
   afterAll(async () => {
